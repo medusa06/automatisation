fa2c8403b6cacd6a2a1ee5cc5f88d61cd3ee35e7
W3DVZA <93668555+medusa06@users.noreply.github.com>
2024-03-28 15:06:23 +0100

    Update backup_dir.sh

--- backup_dir.sh
+++ backup_dir.sh
@@ -6,17 +6,17 @@
 
 
 date_heure=$(date +"%Y-%m-%d_%H-%M-%S")
-dir_save="/home/backups"
-dir_data="/home/data"
+dir_save="backups"
+dir_data="data"
 
-cp -r $dir_data $dir_save"
+cp -r $dir_data $dir_save
 
-taille_dir_save=$(du -sh "$dir_save")
+taille_dir_save=$(du -s "$dir_save" | awk '{print $1}')
 
 if (($taille_dir_save > 5*1024*1024 )) then
-    echo "Attention : la taille du dossier de sauvegarde dépasse 5 Gb."
+    echo "Attention : la taille du dossier de sauvegarde depasse 5 Gb."
 fi
 
-echo "Date et heure de l'exécution du script : $current_date"
-echo "Taille du dossier de sauvegarde : $backup_size"
-echo "Espace disque disponible : $disk_space"
+echo "Date et heure de l'execution du script : "$date_heure
+echo "Taille du dossier de sauvegarde : "$taille_dir_save
+#echo "Espace disque disponible : "$disk_space
