5cb9b171df630b7357b8b7e3bdde5d8e1e1f6ae0
Stormangel83 <bilell.isarti@ynov.com>
2024-03-28 16:44:27 +0100

    sxs

---
+++ config.inc.php
@@ -0,0 +1,9 @@
+<?php
+$config['db_dsnw'] = 'mysql://user:password@mysql_server/roundcube';
+$config['default_host'] = 'localhost';
+$config['smtp_server'] = 'localhost';
+$config['smtp_port'] = 25;
+$config['smtp_user'] = '';
+$config['smtp_pass'] = '';
+$config['support_url'] = '';
+$config['des_key'] = 'someRandomString';
